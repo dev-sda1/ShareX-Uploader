82178d54c1a0706830d8034c75e055c6fa63e1e3
dev-sda1 <cookinggaga@outlook.com>
2021-10-12 00:06:44 +0100

    Custom URL Shortner support.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-## SpotifyWebPresence
-## v1.1 [Python Rewrite]
+## ShareX-Custom-Uploader
+## v1.1
 ## by dev_sda1
 
-## GitHub: https://github.com/dev-sda1/spotify-web-presence
+## GitHub: https://github.com/dev-sda1/ShareX-Uploader
 ## License: GPL-3.0
 
 ## app.py
@@ -14,7 +14,7 @@ import os
 import string
 import random
 from os import access
-from flask import Flask
+from flask import Flask,redirect
 from flask import jsonify
 from flask import request
 from flask_restful import Api, Resource, reqparse
@@ -25,62 +25,95 @@ from werkzeug.utils import send_from_directory
 app = Flask(__name__)
 api = Api(app)
 
-
-@app.post('/upload')
-def uploadContent():
-    #print(request.files)
-    #print(request.form)
-
+def fileHandler(request):
     uploadedFiles = request.files.getlist('sharex')
-    reqParams = request.form
-
+        
     for file in uploadedFiles:
         print(file.filename)
         #file.save(file.filename)
 
     print(uploadedFiles)
-    print(reqParams)
-    
-    # Checking if the secret sent in reqParams matches whats in config.json
 
+    for file in uploadedFiles:
+        # Generate a string for the file name
+
+        # Get the file extension
+        fileExt = file.filename.split('.')[-1]
+        print(fileExt)
+
+        l = string.ascii_lowercase
+        fileName = file.filename
+        fileName = random_string(6)+"."+fileExt
+
+        file.save(os.path.join("./uploads/", fileName))
+        #print("Uploaded " + file.filename)
+            
+        # Return the file name
+        
+    return jsonify(
+        {"file": fileName}
+    )
+
+def urlHandler(request):
+    url = request.form.get('url')
+    
+    # Check if urls.json exists in uploads folder if not create it
+    if not os.path.exists("./uploads/urls.json"):
+        with open("./uploads/urls.json", "w") as f:
+            f.write("{}")
+    
+    # Read the urls.json file
+    with open("./uploads/urls.json", "r") as f:
+        data = json.load(f)
+
+    id = random_string(6)
+    data[id] = url
+
+    # Write the urls.json file
+    with open("./uploads/urls.json", "w") as f:
+        json.dump(data, f)
+
+    # Return the id
+
+    return jsonify(
+        {"file": "go/"+id}
+    )
+
+def random_string(length):  # Generate a random string for use in URLs.
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+@app.post('/upload')
+def uploadContent():
+    print("OwO an upload")
+    r = None
+    reqParams = request.form
+    #print(request.files)
     with open('server/config.json') as json_file:
         data = json.load(json_file)
         secret = data['secret']
 
-    if reqParams['secret'] == secret:
+    if reqParams['secret'] == secret: ## Secret has been matched, upload can commence
         print("Secret Matched")
         
-        # Uploading the file to the server
-        for file in uploadedFiles:
-            # Generate a string for the file name
+        if(request.form['url']) != '':
+            print("URL Shortening")
+            r = urlHandler(request)
+        else:
+            print("File upload")
+            r = fileHandler(request)
 
-            # Get the file extension
-            fileExt = file.filename.split('.')[-1]
-            print(fileExt)
-
-            l = string.ascii_lowercase
-            fileName = file.filename
-            fileName = ''.join(random.choice(l) for i in range(10))+"."+fileExt
-
-            file.save(os.path.join("./uploads/", fileName))
-            #print("Uploaded " + file.filename)
-        
-        # Return the file name
-        return jsonify(
-            {"file": fileName}
-            )
-
-    else:
+    else: ## Secret did not match, 403.
         return(
             jsonify(
                 {"error": "Invalid secret."}
             )
         ),403
 
-    return jsonify(
-        {"message":"ok"}
+    return(
+        r
     )
-
 
 @app.route("/<file_name>")
 def callback(file_name):
@@ -105,10 +138,36 @@ def callback(file_name):
                 {"error": "File not found."}
             ),404
 
+# Shortened URLs
+@app.route("/go/<url_id>")
+def url_route(url_id):
+    url = url_id
+
+    print("Looking for: "+url)
+    if url == "":
+        return jsonify({
+            "error": "No URL specified."
+        }),400
+    else:
+        ## Look for the URL in the urls.json file and return the file
+        with open("./uploads/urls.json", "r") as f:
+            data = json.load(f)
+        
+        if url in data:
+            print("Found URL")
+            
+            # Redirect browser to the URL
+            return redirect(data[url], code=302)
+        else:
+            return jsonify(
+                {"error": "URL not found."}
+            ),404
+
+
 if __name__ == '__main__':
     with open('server/config.json') as json_file:
         data = json.load(json_file)
         portNumber = data['webPort']
 
-    app.run(debug=True,port=portNumber)
+    app.run(debug=True,host="0.0.0.0")
 
--- changelog.md
+++ changelog.md
@@ -1,4 +1,9 @@
 # Changelog
 
+<h1>Version 1.1</h1>
+- URL Shortner support
+- app.py no longer refers to itself as another project
+
+
 <h1>Version 1.0</h1>
 - Initial Release.
